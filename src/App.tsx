f9ed64f1c5743f9afa431b917e15cfd3c2255c3b
yansheng <yanshengluo121@gmail.com>
2025-05-18 17:22:23 -0400

    Integrate TroxlerAdaptation component into App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react';
+import TroxlerAdaptation from './components/TroxlerAdaptation';
+
+function App() {
+  const handleComplete = () => {
+    alert('Adaptation + mask complete!')
+  }
+
+  return (
+    <div className="App" style={{ textAlign: 'center', padding: 20 }}>
+      <h1>Troxler Adaptation Demo</h1>
+      <TroxlerAdaptation
+        width={600}
+        height={400}
+        discCount={10}
+        discDiameterPx={20}
+        eccentricityPx={100}
+        blurPx={4}
+        adaptDurationMs={30000}
+        maskDurationMs={500}
+        onComplete={handleComplete}
+      />
+    </div>
+  )
+}
+
+export default App
